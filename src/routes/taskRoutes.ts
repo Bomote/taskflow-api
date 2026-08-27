4ed9c530e9aa1d5df3baa689f79b099138b20690
Bomote <bomote.chris@gmail.com>
2026-08-27 11:06:39 +0100

    feat: add route protection to task routes using middleware

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -7,13 +7,14 @@ import {
   updateTask,
 } from '../controllers/taskController.ts';
 import { createTaskSchema, updateTaskSchema, validateRequest } from '../utils/validators.ts';
+import { protect } from '../middlewares/protect.ts';
 
 const taskRouter = Router();
 
-taskRouter.get('/', getTasks);
-taskRouter.post('/', validateRequest(createTaskSchema), createTask);
-taskRouter.get('/:id', getTaskById);
-taskRouter.put('/:id', validateRequest(updateTaskSchema), updateTask);
-taskRouter.delete('/:id', deleteTask);
+taskRouter.get('/', protect, getTasks);
+taskRouter.post('/', protect, validateRequest(createTaskSchema), createTask);
+taskRouter.get('/:id', protect, getTaskById);
+taskRouter.put('/:id', protect, validateRequest(updateTaskSchema), updateTask);
+taskRouter.delete('/:id', protect, deleteTask);
 
 export default taskRouter;
