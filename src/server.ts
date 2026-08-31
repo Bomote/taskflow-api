406199668d6cb2d24cab22bc74316213b40b3d26
Bomote <bomote.chris@gmail.com>
2026-08-31 10:54:58 +0100

    feat: add security enhancements with CORS, Helmet, and rate limiting for authentication routes

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,19 @@ import { connectDB } from './config/db.ts';
 import taskRouter from './routes/taskRoutes.ts';
 import { errorHandler } from './middlewares/errorHandler.ts';
 import authRouter from './routes/authRoutes.ts';
+import helmet from 'helmet';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false
+})
 
 async function startServer() {
   try {
@@ -17,13 +27,15 @@ async function startServer() {
   }
 
   app.use(express.json());
+  app.use(cors());
+  app.use(helmet());
 
   app.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok', timestamp: new Date() });
   });
 
   app.use('/api/tasks', taskRouter);
-  app.use('/api/auth', authRouter);
+  app.use('/api/auth', authLimiter, authRouter);
   app.use(errorHandler)
   
   app
